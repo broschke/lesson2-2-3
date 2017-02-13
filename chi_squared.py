a349786feb07972ed9cb3f369fdff6a8d9328f34
broschke <bernardoroschke@gmail.com>
2017-02-13 18:53:08 -0500

    initial commit

---
+++ chi_squared.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
+import collections
+
+# Load the reduced version of the Lending Club Dataset
+loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
+# Drop null rows
+loansData.dropna(inplace=True)
+
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
+
+print(freq)
+
+chi, p = stats.chisquare(list(freq.values()))
+
+print(chi, p)
